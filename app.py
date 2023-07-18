a8629800dabf8d5f5404e41a63bef015ab5b8922
Tirth Shah <tirthshah100@gmail.com>
2023-07-19 00:56:28 +0530

    add files via upload

---
+++ app.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import helper
+import preprocessor
+
+st.sidebar.title("Whatsapp Chat Analyser")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    # fetching unique users
+    users_list = df['user'].unique().tolist()
+    users_list.remove('group_notifications')
+
+    users_list.sort()
+    users_list.insert(0, "Overall")
+    selected_user = st.sidebar.selectbox("Show Analysis With Respect to user:", users_list)
+
+    if st.sidebar.button("Show Analysis"):
+        st.title("Top Statistics")
+
+        num_messages, words, num_media_messages, links = helper.fetch_stats(selected_user, df)
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Total Media")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Total Links")
+            st.title(links)
+
+        # finding the busiest user in the chat
+        if selected_user == 'Overall':
+            st.title("\nMost Active Users")
+            x, new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+
+            col1, col2 = st.columns(2)
+            with col1:
+                ax.bar(x.index, x.values, color='teal')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+
+        # monthly messages timeline
+        st.title("\nMonthly Messages Timeline")
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'], color='red')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # daily messages timeline
+        st.title("\nDaily Messages Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        plt.figure(figsize=(18, 10))
+        ax.plot(daily_timeline['date_num'], daily_timeline['message'], color='green')
+        st.pyplot(fig)
+
+        # weekly analysis
+        st.title("\nMessage Trends Analysis")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.header("Weekdays Trend")
+            busy_day = helper.weekly_analysis(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values, color='violet')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        with col2:
+            st.header("Monthly Trend")
+            busy_month = helper.monthly_analysis(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        # periodic heatmap
+        st.title("\nHourly Heatmap")
+        heat_map = helper.periodic_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
+        ax = sns.heatmap(heat_map)
+        st.pyplot(fig)
+
+        # most common 25 words
+        most_common_df = helper.most_common_words(selected_user, df)
+        st.title('\n20 Most Used Words')
+        fig, ax = plt.subplots()
+        ax.barh(most_common_df[0], most_common_df[1])
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+        # st.dataframe(most_common_df)
+
+        # most common emojis
+        most_emoji_df = helper.most_common_emojis(selected_user, df)
+        st.title("\nMost Used Emojis")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.dataframe(most_emoji_df)
+        with col2:
+            fig, ax = plt.subplots()
+            ax.pie(most_emoji_df[1], labels=most_emoji_df[0], autopct="%0.2f%%", radius=1.5)
+            plt.pie([1], radius=0.75, colors='white')
+            st.pyplot(fig)
+        # fig, ax = plt.subplots()
+        # ax.pie(most_emoji_df[0], most_emoji_df[1])
+        # st.pyplot(fig)
+
+        st.title("\nThankyou for sharing your data. View your processed dataframe:")
+        st.dataframe(df)
+
+        st.text("\nMade by Tirth Shah 😉\n")
---
+++ helper.py
@@ -0,0 +1,107 @@
+from urlextract import URLExtract
+import pandas as pd
+from collections import Counter
+import emoji
+
+
+def fetch_stats(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # fetch number of messages
+    num_messages = df.shape[0]
+
+    # fetch number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # fetch number of media messages
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # fetch number of links
+    extractor = URLExtract()
+    links = []
+    for message in df['message']:
+        links.extend(extractor.find_urls(message))
+
+    return num_messages, len(words), num_media_messages, len(links)
+
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'percent', 'user': 'Name'})
+    return x, df
+
+
+# def createWordCloud(selected_user, df):
+#     if selected_user != 'Overall':
+#         df = df[df['user'] == selected_user]
+#         wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+#         # df_wc = wc.generate(df['message'].str.cat(sep=" "))
+#         corpus = " ".join(df['message'])
+#         df_wc = wc.generate(corpus)
+#         return df_wc
+
+def monthly_timeline(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + " - " + str(timeline['year'][i]))
+
+    timeline['time'] = time
+    return timeline
+
+
+def daily_timeline(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    day_timeline = df.groupby("date_num").count()['message'].reset_index()
+    return day_timeline
+
+
+def weekly_analysis(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    return df['day_num'].value_counts()
+
+
+def monthly_analysis(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    return df['month'].value_counts()
+
+
+def periodic_heatmap(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    user_map = df.pivot_table(index='day_num', columns='period', values='message', aggfunc='count').fillna(0)
+    return user_map
+
+def most_common_words(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    temp = df[df['user'] != 'group_notifications']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+    words = []
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+    df_mc = pd.DataFrame(Counter(words).most_common(20))
+    return df_mc
+
+
+def most_common_emojis(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    emojis = []
+    emojis.extend([match["emoji"] for message in df['message'] for match in emoji.emoji_list(message)])
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(6))
+    return emoji_df
